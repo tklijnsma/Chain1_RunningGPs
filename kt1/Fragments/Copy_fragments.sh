0e3857362db051758dc7e5a74eeb9f8564cf774d
Thomas Klijnsma <klthomas@student.ethz.ch>
2016-04-06 11:23:43 +0200

    Re-added the fragments

---
+++ kt1/Fragments/Copy_fragments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source  /afs/cern.ch/cms/cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc481
+
+cd /afs/cern.ch/work/t/tklijnsm/Production/Chain1_RunningGPs/kt1
+
+if [ -r CMSSW_7_1_14/src ] ; then 
+    echo release CMSSW_7_1_14 already exists
+else
+    scram p CMSSW CMSSW_7_1_14
+fi
+
+mkdir -p CMSSW_7_1_14/src/Configuration/GenProduction/python/
+cp Fragments/* CMSSW_7_1_14/src/Configuration/GenProduction/python/
