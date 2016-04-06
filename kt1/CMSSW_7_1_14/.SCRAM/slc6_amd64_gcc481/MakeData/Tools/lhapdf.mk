00f8b7c354f4f2c2772431d577bb2aa41767b69e
Thomas Klijnsma <klthomas@student.ethz.ch>
2016-04-06 11:22:49 +0200

    Stopped tracking entire CMSSWs

--- kt1/CMSSW_7_1_14/.SCRAM/slc6_amd64_gcc481/MakeData/Tools/lhapdf.mk
+++
@@ -1,5 +0,0 @@
-ALL_TOOLS      += lhapdf
-lhapdf_EX_INCLUDE := /afs/cern.ch/cms/slc6_amd64_gcc481/external/lhapdf/5.9.1-cms3/include
-lhapdf_EX_LIB := LHAPDF
-lhapdf_EX_USE := f77compiler
-
--- kt1/CMSSW_7_1_14/.SCRAM/slc6_amd64_gcc481/MakeData/Tools/sherpa.mk
+++
@@ -1,5 +0,0 @@
-ALL_TOOLS      += sherpa
-sherpa_EX_INCLUDE := /afs/cern.ch/cms/slc6_amd64_gcc481/external/sherpa/2.1.1-cms5/include/SHERPA-MC
-sherpa_EX_LIB := SherpaMain ToolsMath ToolsOrg
-sherpa_EX_USE := hepmc lhapdf qd blackhat fastjet sqlite
-
